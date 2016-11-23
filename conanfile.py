87e123603ec5cfde85f2b8d20f438c5bbff6f4f4
Javier Jerónimo Suárez <jcjeronimo@genexies.net>
2016-11-23 13:51:55 +0100

    Added support for conan.io

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake
+
+
+class FruitConan(ConanFile):
+    name = "Fruit"
+    version = "2.0.4"
+    url = "https://github.com/javierjeronimo/fruit"
+    license = "see https://github.com/javierjeronimo/fruit/blob/master/COPYING"
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+
+    requires = (("Boost/1.60.0@lasote/stable"))
+
+    # No exports necessary
+
+#    def source(self):
+        # this will create a hello subfolder, take it into account
+#        self.run("git clone https://github.com/javierjeronimo/fruit.git")
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+        self.run("make -j")
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy(pattern="*.a", dst="lib", src="src", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="src", keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src="src", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src="src", keep_path=False)
+        self.copy(pattern="*.dylib*", dst="lib", src="src", keep_path=False)
